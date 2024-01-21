088d9d91c72f7080143f08b9c2164cde53e1e3b2
Oscar Mumm <oscarmumm@gmail.com>
2024-01-21 04:30:09 -0300

    trabajando en el componente de selector de colores para asignar a tereas

---
+++ src/components/ColorSelector/ColorSelector.jsx
@@ -0,0 +1,18 @@
+import './ColorSelector.css'
+
+const ColorSelector = () => {
+  return (
+    <div className="color-selector">
+        <div className='color-selector'></div>
+        <div className='color-selector'></div>
+        <div className='color-selector'></div>
+        <div className='color-selector'></div>
+        <div className='color-selector'></div>
+        <div className='color-selector'></div>
+        <div className='color-selector'></div>
+        <div className='color-selector'></div>
+    </div>
+  )
+}
+
+export default ColorSelector
