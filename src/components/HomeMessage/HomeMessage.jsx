e1f74844d75d53277e6d0423d807491891b78dbe
Oscar Mumm <oscarmumm@gmail.com>
2024-01-23 20:32:15 -0300

    corregida alineación de los elementos del home view

--- src/components/HomeMessage/HomeMessage.jsx
+++ src/components/HomeMessage/HomeMessage.jsx
@@ -1,11 +1,20 @@
-import './HomeMessage.css'
+import "./HomeMessage.css";
 
 const HomeMessage = () => {
-    return <div className='home-message'>
-        <p>
-            MyProjects es una aplicación de gestión de proyectos. Te permite crear nuevos proyectos, dividiendolos por etapas, y asignando tareas, que luego podrás mover entre las etapas. 
-        </p>
-    </div>;
+    return (
+        <div className="home-message">
+            <p>
+                MyProjects es la demo de una aplicación de gestión de proyectos.
+                Te permite crear nuevos proyectos, dividiendolos por etapas, y
+                asignando tareas, que luego podrás mover entre las etapas.
+            </p>
+            <br />
+            <p>
+                Atención! La información de los proyectos que crees se eliminará
+                al cerrar la pestaña, ya que solo se trata de una demostración.
+            </p>
+        </div>
+    );
 };
 
 export default HomeMessage;
