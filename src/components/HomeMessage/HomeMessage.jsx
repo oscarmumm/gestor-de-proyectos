0e1a8e4052a799573f41b20c6a72aaf4b798599f
Oscar Mumm <oscarmumm@gmail.com>
2024-01-27 20:29:38 -0300

    corregido mensaje de la página de inicio

--- src/components/HomeMessage/HomeMessage.jsx
+++ src/components/HomeMessage/HomeMessage.jsx
@@ -4,14 +4,13 @@ const HomeMessage = () => {
     return (
         <div className="home-message">
             <p>
-                MyProjects es la demo de una aplicación de gestión de proyectos.
+                MyProjects es la demo de una web app de gestión de proyectos.
                 Te permite crear nuevos proyectos, dividiendolos por etapas, y
                 asignando tareas, que luego podrás mover entre las etapas.
             </p>
             <br />
             <p>
-                Atención! La información de los proyectos que crees se eliminará
-                al cerrar la pestaña, ya que solo se trata de una demostración.
+                La información que maneja esta app se guardará en el local storage de tu navegador. Si deseas puedes instalar la app en tu dispositivo.
             </p>
         </div>
     );
